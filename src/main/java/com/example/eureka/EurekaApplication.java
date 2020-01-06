d5b9cc759a57e1eb9235e45cfab9f41c4b87d990
85 <542574439@qq.com>
2020-01-06 16:20:22 +0800

    服务注册中心

--- src/main/java/com/example/eureka/EurekaApplication.java
+++ src/main/java/com/example/eureka/EurekaApplication.java
@@ -3,11 +3,11 @@ package com.example.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@EnableEurekaClient
+@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
